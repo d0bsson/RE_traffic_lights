25ceb2ebdbbe3bd42ade82b77926e6ef86eb68b2
David <ber.dev@yandex.ru>
2021-08-27 23:20:25 +0300

    Initial Commit

---
+++ RE_traffic_lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RE_traffic_lights
+//
+//  Created by Дэвид Бердников on 27.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
