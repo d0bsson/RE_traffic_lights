28c2230382115408f20cb7856a91d99b7a08e178
David <ber.dev@yandex.ru>
2021-08-28 20:07:10 +0300

    final traffic lights

--- RE_traffic_lights/ViewController.swift
+++ RE_traffic_lights/ViewController.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+enum CurrentLught {
+    case red, yellow, green
+}
+
+
 class ViewController: UIViewController {
+    
     @IBOutlet weak var redLabel: UIView!
     @IBOutlet weak var yellowLabel: UIView!
     @IBOutlet weak var greenLabel: UIView!
@@ -15,23 +21,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private var currentLight = CurrentLught.red
+    private var lightIsOn: CGFloat = 1
+    private var lightIsOff: CGFloat = 0.3
+    
+    override func viewWillLayoutSubviews() {
         redLabel.layer.cornerRadius = redLabel.frame.height / 2
         yellowLabel.layer.cornerRadius = yellowLabel.frame.height / 2
         greenLabel.layer.cornerRadius = greenLabel.frame.height / 2
-        
-        startButton.layer.cornerRadius = 10
-        
-        redLabel.alpha = 0.3
-        yellowLabel.alpha = 0.3
-        greenLabel.alpha = 0.3
-        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLabel.alpha = lightIsOff
+        yellowLabel.alpha = lightIsOff
+        greenLabel.alpha = lightIsOff
     }
 
     @IBAction func startButtonPressed() {
         
+        switch currentLight {
+        case .red:
+            startButton.setTitle("NEXT", for: .normal)
+            
+            currentLight = .yellow
+            redLabel.alpha = lightIsOn
+            greenLabel.alpha = lightIsOff
+        case .yellow:
+            currentLight = .green
+            redLabel.alpha = lightIsOff
+            yellowLabel.alpha = lightIsOn
+        case .green:
+            currentLight = .red
+            greenLabel.alpha = lightIsOn
+            yellowLabel.alpha = lightIsOff
+        }
     }
+    
     
 }
 
