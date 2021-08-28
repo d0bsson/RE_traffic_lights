fccf3a6c570f9e80d142cc520f3f1d05f477ad84
David <ber.dev@yandex.ru>
2021-08-28 17:06:09 +0300

    correction contrains

--- RE_traffic_lights/ViewController.swift
+++ RE_traffic_lights/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLabel.layer.cornerRadius = redLabel.frame.height / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.height / 2
+        greenLabel.layer.cornerRadius = greenLabel.frame.height / 2
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLabel.alpha = 0.3
+        yellowLabel.alpha = 0.3
+        greenLabel.alpha = 0.3
+        
     }
 
-
+    @IBAction func startButtonPressed() {
+        
+    }
+    
 }
 
